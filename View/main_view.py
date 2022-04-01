e1f2a482707aacf1cc7146af330f94f3fcfa41d6
Sylvain Coulibaly <sylvaincoulibaly@yahoo.com>
2022-04-01 19:53:54 +0200

    Réorganisation du code et écriture en LaTex des label des graphiques matplotlib

--- View/main_view.py
+++ View/main_view.py
@@ -16,36 +16,11 @@ class MainView(QMainWindow):
         self._main_controller = main_controller
         self._ui = Ui_MainWindow()
         self._ui.setupUi(self)
+        self.setup_tableview()
         self.setup_connections()
-        self.update_model()
+        self.setup_model()
 
-        #        self._ui.spinBox_L.valueChanged.connect(self.show_L)
-
-        #  self._ui.spinBox_L.valueChanged.connect(self._main_controller.change_L)
-
-        # connect ui-widget to controller
-        # if ui changes, it sends a signal to an slot on which we connect a controller class.
-        # therefore we can receive the signal in the controller
-
-        # self._ui.spinBox_Nx.valueChanged.connect(self._main_controller.change_amount)
-
-        # Lambda to execute function with value
-        # self._ui.spinBox_L.clicked.connect(lambda: self._main_controller.change_amount(0))
-
-        # self._ui.pushButton_add.clicked.connect(lambda: self._main_controller.add_user(self._ui.lineEdit_name.text(
-
-        # listen for model event signals
-        # connect the method to update the ui to the slots of the model
-        # if model sends/emits a signal the ui gets notified
-
-        # self._model.amount_changed.connect(self.on_amount_changed)
-        # self._model.even_odd_changed.connect(self.on_even_odd_changed)
-        # self._model.enable_reset_changed.connect(self.on_enable_reset_changed)
-        #
-        # self._model.users_changed.connect(self.on_list_changed)
-        #
-        # # set a default value
-        # self._main_controller.change_amount(42)
+    def setup_tableview(self):
         self.tableview_1 = self._ui.tableView_initial_states
         self.tableview_1.setModel(self._main_controller.model_wl)
 
@@ -58,15 +33,6 @@ class MainView(QMainWindow):
         self.tableview_2.setItemDelegateForColumn(0, self._main_controller.delegate)
         self.tableview_2.setItemDelegateForColumn(1, self._main_controller.delegate)
         self.tableview_2.setItemDelegateForColumn(2, self._main_controller.delegate)
-
-        self._main_controller.change_type_solver(self._ui.comboBox_type_solver.currentText())
-        self._main_controller.change_problem(
-            self._ui.comboBox_type_solver.currentText())  # on définit le problème en fonction de la combobox
-
-        # self._main_controller.change_l_value(99)  # set a default value
-
-    #        self._main_controller.change_L(1.)
-    # self._main_controller.change_l_value(0)
 
     def setup_connections(self):
         # connect ui-widget to controller
@@ -89,13 +55,10 @@ class MainView(QMainWindow):
 
         self._ui.comboBox_type_solver.currentTextChanged.connect(self._main_controller.change_type_solver)
         self._ui.comboBox_type_solver.currentTextChanged.connect(self._main_controller.change_problem)
-        #  self._ui.btn_run.clicked.connect(self._main_controller.change_problem)
-
-        # self._ui.spinBox_L.valueChanged.connect(self._main_controller.change_problem)
 
         self._ui.btn_run.clicked.connect(self.plot)
 
-    def update_model(self):
+    def setup_model(self):
         # listen for model event signals
         # connect the method to update the ui to the slots of the model
         # if model sends/emits a signal the ui gets notified
@@ -108,6 +71,10 @@ class MainView(QMainWindow):
         # self._model.float_value_changed[float].connect(self._main_controller.change_problem)
         self._main_controller.model_wl.dataChanged.connect(self._main_controller.change_problem2)
         self._main_controller.model_wr.dataChanged.connect(self._main_controller.change_problem2)
+
+        self._main_controller.change_type_solver(self._ui.comboBox_type_solver.currentText())
+        self._main_controller.change_problem(
+            self._ui.comboBox_type_solver.currentText())  # on définit le problème en fonction de la combobox
 
     # self._model.problem_changed[str].connect(self._main_controller.change_problem)
 
@@ -129,25 +96,25 @@ class MainView(QMainWindow):
     def plot_1(self):
         graph = gr.Graphic(self._ui.mycanvas_1,
                            self._model.xm, self._model.r,
-                           x_label="Position xm", y_label="Densité r")
+                           x_label="Position $x_m$", y_label="Densité \u03C1")
         graph.plot()
 
     def plot_2(self):
         graph2 = gr.Graphic(self._ui.mycanvas_2,
                             self._model.xm, self._model.u,
-                            x_label="Position xm", y_label="Vitesse u")
+                            x_label="Position $x_m$", y_label="Vitesse $u$")
         graph2.plot()
 
     def plot_3(self):
         graph3 = gr.Graphic(self._ui.mycanvas_3,
                             self._model.xm, self._model.p,
-                            x_label="Position xm", y_label="Pression p")
+                            x_label="Position $x_m$", y_label="Pression $p$")
         graph3.plot()
 
     def plot_4(self):
         graph4 = gr.Graphic(self._ui.mycanvas_4,
                             self._model.xm, self._model.e,
-                            x_label="Position xm", y_label="Energie interne e")
+                            x_label="Position $x_m$", y_label="Energie interne $e$")
         graph4.plot()
 
     def plot(self):
