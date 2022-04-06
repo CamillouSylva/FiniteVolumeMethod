e722325cdb647bf66884e19bda8ade007987845e
Sylvain Coulibaly <sylvaincoulibaly@yahoo.com>
2022-04-06 11:30:40 +0200

    Ajout des méthodes et connexions (signal -> slot) pour la prise en compte du choix d'un cas test

--- View/main_view.py
+++ View/main_view.py
@@ -15,10 +15,12 @@ class MainView(QMainWindow):
 
         self._main_controller = main_controller
         self._ui = Ui_MainWindow()
+
         self._ui.setupUi(self)
         self.setup_tableview()
         self.setup_connections()
         self.setup_model()
+        self.enable_combobox_test()
 
     def setup_tableview(self):
         self.tableview_1 = self._ui.tableView_initial_states
@@ -56,6 +58,11 @@ class MainView(QMainWindow):
         self._ui.comboBox_type_solver.currentTextChanged.connect(self._main_controller.change_type_solver)
         self._ui.comboBox_type_solver.currentTextChanged.connect(self._main_controller.change_problem)
 
+        self._ui.comboBox_tests.currentIndexChanged.connect(self._main_controller.choose_test_case)
+        self._ui.comboBox_tests.currentIndexChanged.connect(self._main_controller.define_problem)
+
+        self._ui.checkBox.stateChanged.connect(self.enable_combobox_test)
+
         self._ui.btn_run.clicked.connect(self.plot)
 
     def setup_model(self):
@@ -83,6 +90,33 @@ class MainView(QMainWindow):
     # def tableview_updated(self):
     #     self._main_controller.model_wl.dataChaged
 
+    def enable_combobox_test(self):
+        """
+        :return: Méthode appelée lorsqu'on coche ou décoche la QCheckbox.
+        Elle désactive et active les QSpinBox (QDoubleSpinBox) et les
+        QTableView selon l'état de la QCheckBox.
+        """
+        if self._ui.checkBox.isChecked():
+            self._ui.comboBox_tests.setEnabled(True)
+            self._ui.spinBox_L.setEnabled(False)
+            self._ui.spinBox_Nx.setEnabled(False)
+            self._ui.spinBox_x0.setEnabled(False)
+            self._ui.spinBox_T.setEnabled(False)
+            self._ui.spinBox_cfl.setEnabled(False)
+            self._ui.tableView_initial_states.setEnabled(False)
+            self._ui.tableView_initial_states_2.setEnabled(False)
+
+        else:
+            self._ui.comboBox_tests.setEnabled(False)
+            self._ui.spinBox_L.setEnabled(True)
+            self._ui.spinBox_Nx.setEnabled(True)
+            self._ui.spinBox_x0.setEnabled(True)
+            self._ui.spinBox_T.setEnabled(True)
+            self._ui.spinBox_cfl.setEnabled(True)
+            self._ui.tableView_initial_states.setEnabled(True)
+            self._ui.tableView_initial_states_2.setEnabled(True)
+            self._main_controller.change_problem2()
+
     @pyqtSlot(float)
     def on_l_changed(self, value):
         self._ui.spinBox_L.setValue(value)
@@ -107,14 +141,14 @@ class MainView(QMainWindow):
 
     def plot_3(self):
         graph3 = gr.Graphic(self._ui.mycanvas_3,
-                            self._model.xm, self._model.p,
-                            x_label="Position $x_m$", y_label="Pression $p$")
+                            self._model.xm, self._model.e,
+                            x_label="Position $x_m$", y_label="Énergie interne $e$")
         graph3.plot()
 
     def plot_4(self):
         graph4 = gr.Graphic(self._ui.mycanvas_4,
-                            self._model.xm, self._model.e,
-                            x_label="Position $x_m$", y_label="Energie interne $e$")
+                            self._model.xm, self._model.p,
+                            x_label="Position $x_m$", y_label="Pression $p$")
         graph4.plot()
 
     def plot(self):
